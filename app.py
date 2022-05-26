594aad77c14079afcfa782f4d8d39ca021db1a46
Keith Walsh <kwalsh@redhat.com>
2022-05-26 11:48:37 -0400

    Basic error handling to account for intermittent lost connections

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import asyncio
 import os
+import sys
 import time
 
 from dotenv import load_dotenv
 from flask import Flask
 from apis.weather import current_weather
+from kasa.exceptions import SmartDeviceException
 from lib.bulb import Bulb
 from lib.scorer import Scorer
 from lib.signal_handler import SignalHandler
+from pyowm.commons.exceptions import InvalidSSLCertificateError, TimeoutError
 
 load_dotenv()
 
@@ -17,11 +20,17 @@ async def main():
     await bulb.turn_on()
 
     while signal_handler.KEEP_ALIVE:
-        weather_stats = current_weather()
-        scorer = Scorer(weather_stats)
-        await bulb.update_from_score(scorer)
-
-        await report(bulb, weather_stats, scorer)
+        try:
+            weather_stats = current_weather()
+            scorer = Scorer(weather_stats)
+            await bulb.update_from_score(scorer)
+            await report(bulb, weather_stats, scorer)
+        except SmartDeviceException as e:
+            print(f"Failed to connect to bulb with: {e}")
+        except InvalidSSLCertificateError as e:
+            print(f"Failed to connect to weather API with: {e}")
+        except TimeoutError as e:
+            print(f"Connection to weather API timed out with: {e}")
         time.sleep(int(os.getenv("INTERVAL_SECONDS")))
 
     await bulb.turn_off()
